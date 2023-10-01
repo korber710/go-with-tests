83ec3558438dc06de974d8d9c36ca98703e478d5
Steve Korber <korbersa@outlook.com>
2023-10-01 11:35:55 -0400

    Added a new Dictionary type and update the Search method for use with the type.
    Updated and refactored the tests, and added a test to handle the error.

--- pkg/maps/dictionary.go
+++ pkg/maps/dictionary.go
@@ -1,5 +1,16 @@
 package maps
 
-func Search(dictionary map[string]string, word string) string {
-	return dictionary[word]
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	value, ok := d[word]
+	if !ok {
+		return "", ErrWordNotFound
+	}
+
+	return value, nil
 }
+
+var ErrWordNotFound = errors.New("cannot withdraw, insufficient funds")
--- pkg/maps/dictionary_test.go
+++ pkg/maps/dictionary_test.go
@@ -1,17 +1,56 @@
 package maps
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestSearch(t *testing.T) {
+func TestSearchReturnsExpectedStringWithKnownKey(t *testing.T) {
 	t.Parallel()
+	for _, tc := range []dictionaryTestCase{
+		newDictionaryTestCase("test", "this is just a test"),
+		newDictionaryTestCase("hello", "world"),
+	} {
+		tc := tc
+		t.Run(fmt.Sprintf("looking for value '%s' from key '%s'", tc.value, tc.key), func(t *testing.T) {
+			t.Parallel()
+			got, _ := tc.d.Search(tc.key)
+			want := tc.value
+			assert.Equal(t, got, want)
+		})
+	}
+}
 
-	dictionary := map[string]string{"test": "this is just a test"}
-	got := Search(dictionary, "test")
+func TestSearchReportsErrorWithUnknownKey(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []dictionaryTestCase{
+		newDictionaryTestCase("test", "this is just a test"),
+		newDictionaryTestCase("hello", "world"),
+	} {
+		tc := tc
+		t.Run(fmt.Sprintf("expecting error when looking for fake key on %v", tc.d), func(t *testing.T) {
+			t.Parallel()
+			fakeKey := fmt.Sprintf("fake-%s", tc.key)
+			_, err := tc.d.Search(fakeKey)
+			want := ErrWordNotFound
+			assert.Error(t, err)
+			assert.ErrorIs(t, err, want)
+		})
+	}
+}
 
-	want := "this is just a test"
-	assert.Equal(t, got, want)
+type dictionaryTestCase struct {
+	key   string
+	value string
+	d     Dictionary
+}
+
+func newDictionaryTestCase(key, value string) dictionaryTestCase {
+	return dictionaryTestCase{
+		key:   key,
+		value: value,
+		d:     Dictionary{key: value},
+	}
 }
